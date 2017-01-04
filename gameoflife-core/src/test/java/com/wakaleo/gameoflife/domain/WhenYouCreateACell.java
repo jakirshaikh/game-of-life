ce7b02357e035797bf2d106f9c1495cd71cb4491
jakirshaikh <jakirshaikh@outlook.com>
2017-01-04 11:53:14 +0530

    Update WhenYouCreateACell.java

--- gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateACell.java
+++ gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateACell.java
@@ -4,7 +4,7 @@ import static com.wakaleo.gameoflife.domain.Cell.DEAD_CELL;
 import static com.wakaleo.gameoflife.domain.Cell.LIVE_CELL;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
+// just comment
 import org.junit.Test;
 
 public class WhenYouCreateACell {
